5e2eca8fb64c197def29d93ba475a03ba0b832fd
Lêvy Alves <levy.pereiraA1532@gmail.com>
2025-07-28 23:00:39 -0300

    Installing JWT, and configuring authentication and registration routes

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\User\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Auth::user());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $body = $request->only('name', 'email', 'password');
+        User::create($body);
+        return response()->json(['message' => 'User created with success!'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('/v1')->group(function() {
+    Route::post('/auth/login', [AuthController::class, 'login']);
+
+    Route::post('/user', [UserController::class, 'store']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::put('/auth/refresh', [AuthController::class, 'refresh']);
+        Route::delete('/auth/logout', [AuthController::class, 'logout']);
+
+        Route::get('/user', [UserController::class, 'index']);
+    });
+});
